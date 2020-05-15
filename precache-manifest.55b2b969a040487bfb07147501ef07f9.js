f9742013fb0676f029dc298bc679a74e87c826df
boosmoke <lybeck.john@gmail.com>
2020-05-15 16:50:36 -0700

    Updates

---
+++ precache-manifest.55b2b969a040487bfb07147501ef07f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "548b9cb9570720f05bca68da3abf4888",
+    "url": "/product-app/index.html"
+  },
+  {
+    "revision": "77fab6b61572bde60d2c",
+    "url": "/product-app/static/css/main.8175e295.chunk.css"
+  },
+  {
+    "revision": "264cc0f7c632d8c939bc",
+    "url": "/product-app/static/js/2.0ac19c1e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/product-app/static/js/2.0ac19c1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77fab6b61572bde60d2c",
+    "url": "/product-app/static/js/main.6fb591b5.chunk.js"
+  },
+  {
+    "revision": "b682f3441e89d3a1fbf8",
+    "url": "/product-app/static/js/runtime-main.7b6ca8c5.js"
+  }
+]);
